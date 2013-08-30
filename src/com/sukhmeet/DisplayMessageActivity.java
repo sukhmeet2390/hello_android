87f2b700aaa5f4579ebd1b310c77978ad130bfa8
sukhmeet <sukhmeet2390@gmail.com>
2013-08-30 13:57:08 +0530

    Calling different activity , using intents

---
+++ src/com/sukhmeet/DisplayMessageActivity.java
@@ -0,0 +1,29 @@
+package com.sukhmeet;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sukhmeet
+ * Date: 29/08/13
+ * Time: 9:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DisplayMessageActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+
+        // Create Text View
+        TextView textView = new TextView(this);
+        textView.setText(message);
+
+        // set the text view as activity layout
+        setContentView(textView);
+    }
+}
--- src/com/sukhmeet/MyActivity.java
+++ src/com/sukhmeet/MyActivity.java
@@ -1,15 +1,26 @@
 package com.sukhmeet;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MyActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
+    public final static String EXTRA_MESSAGE = "com.sukhmeet.MESSAGE";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
