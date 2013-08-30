4a1645548dadd8a7bafd8e17980c7d1939f3cbfa
sukhmeet <sukhmeet2390@gmail.com>
2013-08-30 15:51:27 +0530

    Action Menu with actions , and UP navigation to parent activity

--- src/com/sukhmeet/MyActivity.java
+++ src/com/sukhmeet/MyActivity.java
@@ -3,6 +3,9 @@ package com.sukhmeet;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -15,7 +18,37 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
     }
+    // to place menu items in action bar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    // define actions for actionsBar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_setting:
+                openSetting();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+    public void openSearch(){
+        System.out.println("In Search ");
+    }
+    public void openSetting(){
+        System.out.println("in Setting");
+    }
+
     public void sendMessage(View view){
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
@@ -23,4 +56,5 @@ public class MyActivity extends Activity {
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
+
 }
